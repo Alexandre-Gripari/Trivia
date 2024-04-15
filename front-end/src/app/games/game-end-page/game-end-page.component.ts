6038c19e3b2a075d1c55a0748adc2a009b2a1c0a
Ilias-Moutaoukil <ilias.moutaoukil@etu.unice.fr>
2024-04-15 10:18:12 +0200

    endPage + nouvelle partie

---
+++ front-end/src/app/games/game-end-page/game-end-page.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-end-page',
+  templateUrl: './game-end-page.component.html',
+  styleUrls: ['./game-end-page.component.scss']
+})
+
+export class GameEndPageComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  }
+
+}
