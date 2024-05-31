fa274a9e5f5388ff9c753d4bbb2c7d5368629ce3
YohanSI3 <yohan.mazzi@etu.unice.fr>
2024-05-31 16:55:19 +0200

    Ajout des users pour le back #56

---
+++ backend/app/api/users/manager.js
@@ -0,0 +1,10 @@
+
+//get all quizzes attached to a user
+const { Quiz } = require('../../models');
+
+const getQuiz = (userId) => {
+    return Quiz.get().filter((quiz) => quiz.userId == userId);
+  }
+
+module.exports = getQuiz;
+  
