9854d327442da14e1a97a98fb0bdb07cf4e19011
Ilias-Moutaoukil <ilias.moutaoukil@etu.unice.fr>
2024-06-18 16:47:33 +0200

    Revert "create/delete user test #77"
    This reverts commit 5554b766108ce5bc547e21039108a7be78749752.

--- front-end/e2e/scenario/createUser-test-scenario.spec.ts
+++ front-end/e2e/scenario/createUser-test-scenario.spec.ts
@@ -1,51 +1,13 @@
 import { test, expect } from '@playwright/test';
-import {homeUrl} from 'e2e/e2e.config';
-import {HomeFixture} from "../../src/app/home/home-container/home-container.fixture";
-import { UserListFixture } from 'src/app/users/user-navigate/user-navigatecomponent.fixture';
-import {UserCardFixture} from "../../src/app/users/user/usercomponent.fixture";
+import { testUserUrl } from 'e2e/e2e.config';
 
 // This file is here to test the playwright integration.
 test.describe('Initial test display', () => {
     test('Verify buttons are visible and clickable', async ({ page }) => {
-        await page.goto(`${homeUrl}`);
+        await page.goto(`${testUserUrl}`);
 
-        const HomeFixtureComponent = new HomeFixture(page);
-
-        await HomeFixtureComponent.clickUserListeButton();
-
-        const userList = await page.locator('.user');
-        const userCount = await userList.count();
-
-        const UserListFixtureComponent = new UserListFixture(page);
-
-        const addButton = UserListFixtureComponent.getAddButton();
-        await expect(addButton).toBeVisible();
-        await UserListFixtureComponent.clickAddButton();
-
-        await page.fill('#first_name', 'Ilias');
-        await page.fill('#last_name', 'Moutaoukil');
-        await page.fill('#birth_date', '2004-08-04');
-        await page.fill('#alzheimerStage', '3');
-        await page.click('button[type="submit"]');
-
-        await page.reload();
-
-        const newUserCount = await page.locator('.user').count();
-
-        if (userCount + 1 == newUserCount) {
-            console.log('User added successfully');
-        } else {
-            console.log('User not added');
-        }
-
-        const userList2 = await page.locator('.user');
-        const user = userList2.nth(1);
-        await user.hover();
-
-        await page.getByRole('button', { name: 'MODIFIER' }).nth(1).click();
-
-        await page.click('button[type="button"]');
-
-        await page.reload();
-    });
+        
+    
+        
+      });
 });
