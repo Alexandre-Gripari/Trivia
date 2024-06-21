93ba55cc607bab58fc50ad37cbc6c63adda56bf9
Alexandre-Gripari <alexandre.gripari@etu.univ-cotedazur.fr>
2024-06-21 10:54:25 +0200

    Etape3 avec 2 Dockerfile

--- front-end/e2e/e2e.config.ts
+++ front-end/e2e/e2e.config.ts
@@ -1,4 +1,6 @@
-export const testUserUrl = 'http://localhost:4200/user-page';
-export const testGameUrl = 'http://localhost:4200/game-page';
-export const homeUrl = 'http://localhost:4200/home-page';
-export const testStatsUrl = 'http://localhost:4200/statistic-page';
+export const testUrl = process.env['TEST_URL'] || 'http://localhost:4200';
+
+export const testUserUrl = testUrl + '/user-page';
+export const testGameUrl = testUrl +'/game-page';
+export const homeUrl = testUrl + '/home-page';
+export const testStatsUrl = testUrl + '/statistic-page';
